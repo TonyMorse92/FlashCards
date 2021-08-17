fdd8962fe8cf3e948710bf3017143f2b43ff1a76
TonyMorse92 <anthonymorse92@gmail.com>
2021-08-17 13:45:49 -0400

    Frontent functionality on the right track. Ran into issue with MathJax not rerendering.

---
+++ frontend/js/add_card.js
@@ -0,0 +1,60 @@
+/*
+ * Sends the new flashcard information to the Spring Boot server
+ *
+*/
+
+var postRequest = new XMLHttpRequest();
+postRequest.open("POST", 'http://localhost:8080/flashcard-app/api/flashcards/', true);
+postRequest.setRequestHeader('Content-Type', 'application/json');
+
+//const addButton = document.getElementById("add-button");
+//addButton.onclick = addFlashcard;
+
+function addFlashcard()
+{
+	const subject = document.getElementById('subject').value;
+	const question = document.getElementById('question').value;
+	const answer = document.getElementById('answer').value;
+	
+	if(validateData())
+	{
+		//alert(JSON.stringify({subject:subject, question:question, answer:answer}));
+		postRequest.send(JSON.stringify({subject:subject, question:question, answer:answer}));
+		clear();
+		confirm();
+	}
+	else
+	{
+		alert("No data field can be empty!");
+	}
+}
+
+function validateData()
+{
+	if(document.getElementById('subject').value == '')
+		return false;
+	if(document.getElementById('question').value == '')
+		return false;
+	if(document.getElementById('answer').value == '')
+		return false;
+	return true;
+}
+
+function clear()
+{
+	document.getElementById('subject').value = '';
+	document.getElementById('question').value = '';
+	document.getElementById('answer').value = '';
+}
+
+function confirm()
+{
+	alert("Data sent successfully!");
+}
+
+function returnToPrevPage()
+{	
+	window.history.back();
+}
+
+
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -13,13 +13,21 @@ var request = new XMLHttpRequest();
 request.open('GET', 'http://localhost:8080/flashcard-app/api/flashcards/', true);
 
 
-alert("Requested flashcards.");
-alert("Request:  " + request);
-alert("Request Status: " + request.status);
+
+//alert("Requested flashcards.");
+//alert("Request:  " + request);
+
 
 request.onload = function () 
 {
+	//alert("Request Status: " + request.status);
 	var data = JSON.parse(this.response);
+	
+	// The first flashcard shown is going to be random
+	// Current thought is to have all of them be random, because going through the 
+	// same flashcard multiple times is probably a good thing.
+	// random number is between 0 and number of (flashcards - 1)
+	let randomNumber = Math.floor(Math.random() * data.length)
 
 	if (request.status >= 200 && request.status < 400) 
 	{
@@ -37,9 +45,11 @@ request.onload = function ()
 		topButtonRow.appendChild(addButton);
 		topButtonRow.appendChild(updateButton);
 
+		//let flashcard = data[randomNumber];
 		let flashcard = data[0];
 		const card = document.createElement('div');
 		card.setAttribute('class', 'card');
+		card.setAttribute('id', 'card');
 
 		const h1 = document.createElement('h1');
 		h1.textContent = flashcard.subject;
@@ -76,6 +86,43 @@ request.onload = function ()
 		app.appendChild(errorMessage);
 	}
 
+	renderPt1();
+	renderPt2();
 }
+
+function renderPt1() 
+{
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src  = "https://polyfill.io/v3/polyfill.min.js?features=es6";	
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+function renderPt2()
+{
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+script.src  = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+//document.getElementById('add-button').onclick = buttonAction('add-button');
+
+/*function buttonAction(id)
+{
+	if(id == 'add-button')
+	{
+		location.href = "add_card.html";
+	}
+	else if(id == 'update-button')
+	{
+	}
+	else if(id == 'next-button')
+	{
+	}
+	else if(id == 'flip-button')
+	{
+	}
+}*/
+
 
 request.send();
